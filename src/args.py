4c6d30a9752e28b9baf7a0966c0b0df5cf0706c7
melih-unsal <melihunsal.ai@gmail.com>
2023-10-28 01:02:19 +0300

    param changed

--- src/args.py
+++ src/args.py
@@ -2,7 +2,7 @@ TARGET_COLUMN_THRESHOLD = 20            # it is used to decide for the algorithm
 HIGH_TARGET_COLUMN_MAPPING = 30         # it is used to decide for the example count for CellModel
 
 ROW_MODEL_FEW_SHOT_COUNT_SMALL = 6      # sets the number of few shot prompts for small tables
-ROW_MODEL_PERCENTAGE_SMALL = 1.0        # sets the remaining columns percentage after randomly removing them for small tables.
+ROW_MODEL_PERCENTAGE_SMALL = 0.6        # sets the remaining columns percentage after randomly removing them for small tables.
 ROW_MODEL_FEW_SHOT_COUNT_BIG = 3        # sets the number of few shot prompts for big tables
 ROW_MODEL_PERCENTAGE_BIG = 0.8          # sets the remaining columns percentage after randomly removing them for big tables.
 
